5114dd491ad77d7fea0ad5f8b17c0c506217a74f
yjun0915 <kyjun09150916@gmail.com>
2025-03-29 19:17:42 +0900

    read and write serial signal

--- Python/serial_tutorial.py
+++ Python/serial_tutorial.py
@@ -4,7 +4,7 @@ import time
 py_serial = serial.Serial(
 
     # Window
-    port='COM3',
+    port='/dev/cu.usbmodem1101',
 
     # 보드 레이트 (통신 속도)
     baudrate=9600,
@@ -19,9 +19,10 @@ while True:
     time.sleep(0.1)
 
     if py_serial.readable():
-        # 들어온 값이 있으면 값을 한 줄 읽음 (BYTE 단위로 받은 상태)
-        # BYTE 단위로 받은 response 모습 : b'\xec\x97\x86\xec\x9d\x8c\r\n'
-        response = py_serial.readline()
-
-        # 디코딩 후, 출력 (가장 끝의 \n을 없애주기위해 슬라이싱 사용)
-        print(response[:len(response) - 1].decode())
+        print(1)
+        # # 들어온 값이 있으면 값을 한 줄 읽음 (BYTE 단위로 받은 상태)
+        # # BYTE 단위로 받은 response 모습 : b'\xec\x97\x86\xec\x9d\x8c\r\n'
+        # response = py_serial.readline()
+        #
+        # # 디코딩 후, 출력 (가장 끝의 \n을 없애주기위해 슬라이싱 사용)
+        # print(response[:len(response) - 1].decode())
