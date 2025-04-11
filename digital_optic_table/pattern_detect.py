5fc18bda700c240b6fc62e23f5b40ec668a3b7cc
yjun0915 <kyjun09150916@gmail.com>
2025-04-11 12:46:37 +0900

    circle radius estimation for convolution reference

--- digital_optic_table/pattern_detect.py
+++ digital_optic_table/pattern_detect.py
@@ -19,6 +19,9 @@ image = cv2.imread('IMG_0126.jpg', cv2.IMREAD_COLOR)
 if image is None:
     raise FileNotFoundError("이미지를 찾을 수 없습니다. 경로를 확인하세요.")
 
+fig1 = plt.figure(1)
+fig2 = plt.figure(2)
+
 img_ratio = max(np.shape(image)[1]/monitor.width, np.shape(image)[0]/monitor.height)
 image = cv2.resize(src=image, dsize=(int(np.shape(image)[1]/img_ratio), int(np.shape(image)[0]/img_ratio)))
 
@@ -27,7 +30,7 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 canny_image = cv2.Canny(image=gray_image, threshold1=120, threshold2=130, apertureSize=3, L2gradient=True, )
 #canny_image_2 = cv2.Canny(image=gray_image, threshold1=120, threshold2=200, apertureSize=5, L2gradient=True, )
 
-lines = cv2.HoughLinesP(image=canny_image, rho=10, theta=np.pi/360, threshold=160, minLineLength=0, maxLineGap=0)
+lines = cv2.HoughLinesP(image=canny_image, rho=10, theta=np.pi/3600, threshold=160, minLineLength=0, maxLineGap=0)
 '''
 cv2.HoughLinesP(image, rho, theta, threshold, lines=None, minLineLength=None, maxLineGap=None) -> lines
 - image: 입력 에지 영상
@@ -54,27 +57,60 @@ if lines is not None:
                 cv2.line(hough_image, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
                 cv2.line(dot_map, pt1, pt2, 1, 2, cv2.LINE_AA)
 
-radius = 15
-circle = np.zeros(shape=(radius*2, radius*2))
-cv2.circle(img=circle, center=(radius-1, radius-1), radius=radius, color=[1], thickness=1)
 
-convoluted_map = cv2.filter2D(src=dot_map, ddepth=-1, kernel=circle, delta = -10)/280
-gaussian_map = cv2.GaussianBlur(src=convoluted_map, ksize=(5, 5), sigmaX=0.1)*10
-gaussian_map = gaussian_map.astype(np.int64)*1.0
+
+max_radius = 30
+score = np.zeros(shape=(max_radius, 2))
+cv2.namedWindow(winname="convoluting")
+for radi in range(1, max_radius):
+    circle = np.zeros(shape=(radi*2, radi*2))
+    cv2.circle(img=circle, center=(radi-1, radi-1), radius=radi, color=[1], thickness=1)
+
+    convoluted_map = cv2.filter2D(src=canny_image/255, ddepth=-1, kernel=circle, delta = -10)/280
+    gaussian_map = cv2.GaussianBlur(src=convoluted_map, ksize=(5, 5), sigmaX=0.1)*10
+    gaussian_map = gaussian_map.astype(np.int64)*1.0
+
+    score[radi] = [radi, np.sum(gaussian_map)]
+
+    print("[%3d]"%radi, "Sum of convoluted map :", score[radi][1])
+    cv2.imshow(winname="convoluting", mat=gaussian_map)
+    cv2.waitKey(10)
+ax1 = fig1.add_subplot(111)
+ax1.plot(score)
+
+plt.waitforbuttonpress()
+radius = int(score[score.argmax(axis=0)[1]][0])
+print("Estimated radius of holes :", radius)
+
+
+
+circle = np.zeros(shape=(radius * 2, radius * 2))
+cv2.circle(img=circle, center=(radius - 1, radius - 1), radius=radius, color=[1], thickness=1)
+
+convoluted_map = cv2.filter2D(src=canny_image/255, ddepth=-1, kernel=circle, delta=-10) / 280
+gaussian_map = cv2.GaussianBlur(src=convoluted_map, ksize=(5, 5), sigmaX=0.1) * 10
+gaussian_map = gaussian_map.astype(np.int64) * 1.0
 
 dft = cv2.dft(src=np.float32(gaussian_map), flags=cv2.DFT_COMPLEX_OUTPUT)
 dft_shift = np.fft.fftshift(dft)
 
 magnitude_spectrum = 0.2*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
 
-fft_plot = smooth(magnitude_spectrum[0, :], 1)
-plt.plot(fft_plot)
+for ix in range(int((np.shape(magnitude_spectrum)[0] - 2*radius)/2),
+                int((np.shape(magnitude_spectrum)[0] + 2*radius)/2)):
+    for iy in range(int((np.shape(magnitude_spectrum)[1] - 2*radius) / 2),
+                    int((np.shape(magnitude_spectrum)[1] + 2*radius) / 2)):
+        magnitude_spectrum[ix][iy] = 0
+
+fft_plot = smooth(magnitude_spectrum[int(np.shape(magnitude_spectrum)[0]/2), :], 12)
+ax2 = fig2.add_subplot(111)
+ax2.plot(fft_plot)
 
 max_point = np.unravel_index(np.argmax(gaussian_map, axis=None), gaussian_map.shape)
 estimated_max_point = np.unravel_index(np.argmax(convoluted_map, axis=None), convoluted_map.shape)
-print(estimated_max_point)
+#print(estimated_max_point)
 estimated_max_point = [estimated_max_point[0]-int(np.shape(magnitude_spectrum)[0]/2), estimated_max_point[1]-int(np.shape(magnitude_spectrum)[1]/2)]
-print(estimated_max_point)
+#print(estimated_max_point)
 
 cv2.circle(img=gaussian_map, center=max_point, radius=3, color=[255, 0, 255], thickness=2)
 cv2.circle(img=gaussian_map, center=(max_point[0]+estimated_max_point[0], max_point[1]+estimated_max_point[1]), radius=3, color=[255, 255, 0], thickness=1)
@@ -84,7 +120,7 @@ cv2.imshow(winname="spec", mat=magnitude_spectrum)
 
 cv2.imshow(winname="convoluted", mat=gaussian_map)
 
-#cv2.imshow(winname="fig_0", mat=dot_map)
+cv2.imshow(winname="fig_0", mat=dot_map)
 #cv2.imshow(winname="fig_00", mat=hough_image)
 plt.show()
 cv2.waitKey()
