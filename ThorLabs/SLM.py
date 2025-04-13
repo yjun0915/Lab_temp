e4d210f0a48d9ba2e721e24a5235070ab1c92d7a
yjun0915 <kyjun09150916@gmail.com>
2025-04-14 08:54:49 +0900

    get new result

--- ThorLabs/SLM.py
+++ ThorLabs/SLM.py
@@ -3,3 +3,9 @@ import numpy as np
 
 arr = np.zeros(shape=(100, 100))
 
+x = np.linspace(start=-10, stop=10, num=100)
+y = x
+
+X, Y = np.meshgrid(x, y)
+
+cv2.imshow()
--- TimeTagger/data_to_fig.py
+++ TimeTagger/data_to_fig.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import re
 from scipy.optimize import curve_fit
 
-Channel_name = ['Channel A', 'Channel B', 'Coincidence']
+Channel_name = ['Channel A', 'Channel B', 'Coincidence', 'True Coincidence']
 resolution = 20
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 bin_width = 0.1
@@ -22,10 +22,9 @@ def gaussian(x, a, mu, sigma):
 
 
 # 파일 경로
-file_path = "CoincidenceExampleData_16.txt"
+file_path = "CoincidenceExampleData_30.txt"
 fig = plt.figure(num=1, figsize=(16, 9))
 fig1 = plt.figure(num=2, figsize=(14, 7))
-fig2 = plt.figure(num=3, figsize=(14, 7))
 
 # 데이터 읽기
 with open(file_path, "r") as file:
@@ -62,17 +61,18 @@ ax1 = fig.add_subplot(2, 5, (1, 4))
 accidental = 0
 real_coin = 0
 for i, row in enumerate(counter_data):
+    row = row[-21:-1]
     if i == 0:
         accidental = row
         real_coin = row
     elif i == 1: accidental = accidental*row
     elif i == 2: real_coin = row - accidental*cw*1e-12
-    ax1.plot((np.arange(len(row))-len(row))/(bin_width*np.shape(counter_data)[1]), row, 'o-')
-ax1.plot((np.arange(len(row))-len(row))/(bin_width*np.shape(counter_data)[1]), real_coin, 'o-')
+    ax1.plot((np.arange(len(row)))/(bin_width*np.shape(counter_data)[1]), row, 'o-')
+ax1.plot((np.arange(len(row)))/(bin_width*np.shape(counter_data)[1]), real_coin, 'o-')
 ax1.set_xlabel('Time (s)')
 ax1.set_ylabel('Count')
 ax1.set_title('Counter Data')
-ax1.legend((Channel_name[0], Channel_name[1], Channel_name[2], "Real coincidence"), loc=7)
+ax1.legend((Channel_name[0], Channel_name[1], Channel_name[2], Channel_name[3]), loc=7)
 ax1.grid(True)
 fig.subplots_adjust(left=0.125, bottom=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.3)
 
@@ -105,42 +105,50 @@ ax4.axis('off')
 
 
 _ax1 = fig1.add_subplot(111)
+A=0
+B=0
 accidental = 0
 coin = 0
 real_coin = 0
 for i, row in enumerate(counter_data):
+    row = row[-21:-1]
     if i == 0:
+        A = row
         accidental = row
         real_coin = row
-    elif i == 1: accidental = accidental*row
+    elif i == 1:
+        B = row
+        accidental = accidental*row
     elif i == 2:
         coin = row
         real_coin = row - accidental*cw*1e-12
-    _ax1.plot((np.arange(len(row))-len(row))/(bin_width*np.shape(counter_data)[1]), row, 'o-')
-_ax1.plot((np.arange(len(row))-len(row))/(bin_width*np.shape(counter_data)[1]), real_coin, 'o-')
+    _ax1.plot((np.arange(len(row)))/(bin_width*np.shape(counter_data)[1]), row, 'o-')
+
+over30points = np.zeros(shape=np.shape(A))
+for idx in range(np.shape(A)[0]):
+    if 0.3 <= (real_coin[idx])/np.sqrt(A[idx]*B[idx]):
+        over30points[idx] = 1
+
+_ax1.plot((np.arange(len(row)))/(bin_width*np.shape(counter_data)[1]), real_coin, 'o-')
+
+# for idx in range(np.shape(over30points)[0]):
+#     if over30points[idx] == 1:
+#         _ax1.scatter(x=(idx-len(row))/(bin_width*np.shape(counter_data)[1]), y=A[idx], c="b", zorder=10)
+#         _ax1.scatter(x=(idx-len(row))/(bin_width*np.shape(counter_data)[1]), y=B[idx], c="b", zorder=10)
+#         _ax1.scatter(x=(idx-len(row))/(bin_width*np.shape(counter_data)[1]), y=real_coin[idx], c="b", zorder=10)
+
 _ax1.set_xlabel('Time (s)')
-_ax1.set_xlim(xmax=0)
+_ax1.set_xlim(-0.1, len(row)/(bin_width*np.shape(counter_data)[1]))
 _ax1.set_ylabel('Count')
 _ax1.set_title('Counter Data')
-_ax1.legend((Channel_name[0], Channel_name[1], Channel_name[2], "Real coincidence"), loc=7)
+_ax1.legend((Channel_name[0], Channel_name[1], Channel_name[2], Channel_name[3]), loc=7)
 for i, row in enumerate(counter_data):
+    row = row[-21:-1]
     ave = np.average(row[-_n:-1])
-    _ax1.hlines(y=ave, xmin=-len(row[-_n:-1])/(bin_width*np.shape(counter_data)[1]), xmax=1, colors='k')
-    _ax1.text(x=0.1, y=float(ave-100), s="← "+"{:.3f}".format(ave), fontdict=font)
+    _ax1.hlines(y=ave, xmin=0, xmax=len(row)/(bin_width*np.shape(counter_data)[1]), colors='k')
+    _ax1.text(x=len(row)/(bin_width*np.shape(counter_data)[1]), y=float(ave-58), s="← "+"{:.3f}".format(ave), fontdict=font)
 _ax1.grid(True)
 fig1.subplots_adjust(left=0.1, bottom=0.1, right=0.8, top=0.9, wspace=0.3, hspace=0.3)
-
-_ax2 = fig2.add_subplot(111)
-_ax2.plot((np.arange(len(row))-len(row))/(bin_width*np.shape(counter_data)[1]), coin - real_coin, 'o-')
-print(np.average(coin - real_coin))
-_ax2.grid(True)
-_ax2.set_xlabel('Time (s)')
-_ax2.set_xlim(xmax=0)
-_ax2.set_ylim(ymin=0, ymax = np.max(coin - real_coin)*1.5)
-_ax2.set_ylabel('Difference')
-_ax2.set_title('Coincidence Counts - Real Coincidence Counts')
-#_ax2.legend("(coinsidence counts) - (real coincidence counts)", loc=0)
-fig2.subplots_adjust(left=0.1, bottom=0.1, right=0.8, top=0.9, wspace=0.3, hspace=0.3)
 
 plt.show()
 fig.savefig(fname='result fig.png')
