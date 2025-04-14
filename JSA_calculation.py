7f5efb7476cbc9a34d6dc24ed7d6148765c8e17a
yjun0915 <kyjun09150916@gmail.com>
2025-04-14 17:48:46 +0900

    fix coefficients

--- JSA_calculation.py
+++ JSA_calculation.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+import os
+
+pwd = os.path.realpath(__file__)
+print(pwd)
 
 temperature = 20
 
-domain = (750, 820)
-step = 5000
+domain = (700, 900)
+step = 1000
 figure_ticks = 4
 
 def tick_setter(_ticks, axis_data):
@@ -18,29 +23,39 @@ c = 299792458 * 1e9 / n
 wavelength_signal = np.linspace(start=domain[0], stop=domain[1], num=step)
 wavelength_idler = np.linspace(start=domain[0], stop=domain[1], num=step)
 
-wavelength_pump = float(input("Wavelength of Pump (nm) :"))
-bandwidth_pump = float(input("Gaussian distributed Pump spectrum bandwidth (nm) :"))
-poling_period = float(input("Poling period of the SPDC crystal (㎛) :")) * 1e3
-L = float(input("length of the SPDC crystal (mm) :")) * 1e6
+val = [0, 0, 0, 0]
 
-frequency_signal = np.reciprocal(wavelength_signal) * (2*np.pi*c)
-frequency_idler = np.reciprocal(wavelength_idler) * (2*np.pi*c)
+if os.path.isfile('setting_info.csv'):
+    with open('setting_info.csv', 'r', encoding='cp949') as file:
+        csv_reader = csv.reader(file)
+        idx = 0
+        for row in csv_reader:
+            val[idx] = float(row[1])
+            idx += 1
+
+wavelength_pump = float(input("Wavelength of Pump (nm) :") or val[0])
+bandwidth_pump = float(input("Gaussian distributed Pump spectrum bandwidth (nm) :") or val[1])
+poling_period = float(input("Poling period of the SPDC crystal (㎛) :") or val[2]) * 1e3
+L = float(input("length of the SPDC crystal (mm) :") or val[3]) * 1e6
+
+frequency_signal = np.reciprocal(wavelength_signal) * (2*np.pi)
+frequency_idler = np.reciprocal(wavelength_idler) * (2*np.pi)
 frequency_pump =  (2*np.pi*c)/wavelength_pump
 
 X, Y = np.meshgrid(frequency_signal, frequency_idler)
-amplitude = np.exp(-1 * np.pow((X + Y - frequency_pump)/bandwidth_pump, 2))
+amplitude = np.exp(-1 * np.pow((X + Y - frequency_pump)/(2*np.pi/bandwidth_pump), 2))
 
-delta_k = ((frequency_pump - X - Y)/c) + (2*np.pi/poling_period)
+delta_k = ((frequency_pump - X - Y)/(2*np.pi)) + (2*np.pi/(2*np.pi*poling_period))
 phase_matching_amplitude = np.sinc(delta_k*L/2)
 
-plt.pcolor(phase_matching_amplitude)
+plt.pcolor(amplitude*phase_matching_amplitude)
 ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_signal)
 plt.xticks(ticks=ticks, labels=labels)
 ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_idler)
 plt.yticks(ticks=ticks, labels=labels)
 plt.xlabel("Signal wavelength (nm)")
 plt.ylabel("Idler wavelength (nm)")
-plt.clim(0., 0.1)
+#plt.clim(0., 1)
 plt.colorbar()
 
 plt.show()
