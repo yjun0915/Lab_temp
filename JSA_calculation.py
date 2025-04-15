dc851719c9ede23739e7bcbf3bf449cc4e61a159
yjun0915 <kyjun09150916@gmail.com>
2025-04-15 18:19:52 +0900

    try for new data

--- JSA_calculation.py
+++ JSA_calculation.py
@@ -1,15 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import csv
 import os
 
 pwd = os.path.realpath(__file__)
-print(pwd)
+fig = plt.figure(num=1, figsize=(13, 4))
+grids = gridspec.GridSpecFromSubplotSpec(nrows=1, ncols=4, width_ratios=[10, 10, 10, 0.5], subplot_spec=gridspec.GridSpec(nrows=1, ncols=1)[0], wspace=0.05)
 
 temperature = 20
 
-domain = (700, 900)
-step = 1000
+domain = (1950, 2000)
+step = 500
 figure_ticks = 4
 
 def tick_setter(_ticks, axis_data):
@@ -17,8 +19,8 @@ def tick_setter(_ticks, axis_data):
     arrange = np.arange(length//(2*_ticks), length, length//_ticks)
     return arrange, axis_data[arrange].astype(np.float16)
 
-n = 1
-c = 299792458 * 1e9 / n
+n = 1               # non-dimension
+c = 299792458 / n   # meter per second
 
 wavelength_signal = np.linspace(start=domain[0], stop=domain[1], num=step)
 wavelength_idler = np.linspace(start=domain[0], stop=domain[1], num=step)
@@ -33,29 +35,48 @@ if os.path.isfile('setting_info.csv'):
             val[idx] = float(row[1])
             idx += 1
 
-wavelength_pump = float(input("Wavelength of Pump (nm) :") or val[0])
-bandwidth_pump = float(input("Gaussian distributed Pump spectrum bandwidth (nm) :") or val[1])
-poling_period = float(input("Poling period of the SPDC crystal (㎛) :") or val[2]) * 1e3
-L = float(input("length of the SPDC crystal (mm) :") or val[3]) * 1e6
+wavelength_pump = float(input("Wavelength of Pump (nm) :") or val[0]) * 1e-9                            # meter
+bandwidth_pump = float(input("Gaussian distributed Pump spectrum bandwidth (nm) :") or val[1]) * 1e-9   # meter
+poling_period = float(input("Poling period of the SPDC crystal (㎛) :") or val[2]) * 1e-6                # meter
+L = float(input("length of the SPDC crystal (mm) :") or val[3]) * 1e-3                                  # meter
 
-frequency_signal = np.reciprocal(wavelength_signal) * (2*np.pi)
-frequency_idler = np.reciprocal(wavelength_idler) * (2*np.pi)
-frequency_pump =  (2*np.pi*c)/wavelength_pump
+print(val)
 
-X, Y = np.meshgrid(frequency_signal, frequency_idler)
-amplitude = np.exp(-1 * np.pow((X + Y - frequency_pump)/(2*np.pi/bandwidth_pump), 2))
+frequency_signal = np.reciprocal(wavelength_signal) * (2*np.pi*c)           # radian per second (angular frequency)
+frequency_idler = np.reciprocal(wavelength_idler) * (2*np.pi*c)             # radian per second (angular frequency)
+frequency_pump =  (2*np.pi*c)/wavelength_pump                               # radian per second (angular frequency)
 
-delta_k = ((frequency_pump - X - Y)/(2*np.pi)) + (2*np.pi/(2*np.pi*poling_period))
+X, Y = np.meshgrid(frequency_signal, frequency_idler)       # 2-dimensional, radian per second  (angular frequency space)
+amplitude = np.exp(-1 * np.pow((X + Y - frequency_pump)/(bandwidth_pump), 2))
+
+delta_k = ((frequency_pump - X - Y)/(2*np.pi*c)) + (2*np.pi/poling_period)
 phase_matching_amplitude = np.sinc(delta_k*L/2)
 
-plt.pcolor(amplitude*phase_matching_amplitude)
+ax1 = fig.add_subplot(grids[0])
+plt.pcolor(amplitude)
 ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_signal)
 plt.xticks(ticks=ticks, labels=labels)
 ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_idler)
 plt.yticks(ticks=ticks, labels=labels)
 plt.xlabel("Signal wavelength (nm)")
 plt.ylabel("Idler wavelength (nm)")
-#plt.clim(0., 1)
-plt.colorbar()
+
+ax2 = fig.add_subplot(grids[1])
+plt.pcolor(phase_matching_amplitude)
+ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_signal)
+plt.xticks(ticks=ticks, labels=labels)
+plt.xlabel("Signal wavelength (nm)")
+plt.yticks([])
+
+ax3 = fig.add_subplot(grids[2])
+plt.pcolor(amplitude*phase_matching_amplitude)
+ticks, labels = tick_setter(_ticks=figure_ticks, axis_data=wavelength_signal)
+plt.xticks(ticks=ticks, labels=labels)
+plt.xlabel("Signal wavelength (nm)")
+plt.yticks([])
+
+color_axis = fig.add_subplot(grids[3])
+plt.colorbar(cax=color_axis)
+fig.tight_layout()
 
 plt.show()
