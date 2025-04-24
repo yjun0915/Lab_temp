d15957bc217eb19ec94e6769b298f97f6db295e1
yjun0915 <kyjun09150916@gmail.com>
2025-04-24 10:12:45 +0900

    error fix note

--- ThorLabs/kinesis.py
+++ ThorLabs/kinesis.py
@@ -1,5 +1,11 @@
 from pylablib.devices import Thorlabs
 
+'''
+Important prechanges
+.venv/Lib/site-packages/ft232/__init__.py [line 19~21] : .d2xx -> d2xx
+.venv/Lib/site-packages/pylablib/devices/Thorlabs/kinesis.py [line 101] : FT232DeviceBackend -> HIDeviceBackend
+'''
+
 device_list = Thorlabs.list_kinesis_devices()
 
 selection = device_list[int(input(device_list))][0]
