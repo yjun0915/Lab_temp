801107fc09293ceab55abc3c1ef4eab40c1adb92
yjun0915 <kyjun09150916@gmail.com>
2025-04-24 09:31:08 +0900

    kinesis motor controlling code, device attatching

--- ThorLabs/kinesis.py
+++ ThorLabs/kinesis.py
@@ -1,2 +1,11 @@
 from pylablib.devices import Thorlabs
 
+device_list = Thorlabs.list_kinesis_devices()
+
+selection = device_list[int(input(device_list))][0]
+
+stage = Thorlabs.KinesisMotor(selection)
+
+
+
+stage.close()
