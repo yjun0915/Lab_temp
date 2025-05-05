439ef945e780eb18e13128090433da3fdcaaa2bf
yjun0915 <kyjun09150916@gmail.com>
2025-05-05 14:03:30 +0900

    visibility display label

--- ThorLabs/data/figure.py
+++ ThorLabs/data/figure.py
@@ -7,6 +7,9 @@ from scipy.optimize import curve_fit
 import matplotlib
 matplotlib.use('TkAgg')
 
+visibility = 0.0
+fig, ax = plt.subplots(3)
+
 
 def v_line(x, a, b, c, d, h):
     output_list = []
@@ -48,7 +51,7 @@ def make_figure(get_selection):
     position_log.plot(kind='line', y='position', ax=ax[2], lw=0.5)
     position_log.reset_index().plot(kind='scatter', x='index', y='position', ax=ax[2], s=0.1)
 
-    p0 = [0, 2000, 1, 0, 1000]
+    p0 = [0, measurement['coincidence counts'].mean(), 1, 0, measurement['coincidence counts'].min()]
     coeff, var_matrix = curve_fit(f=v_line, xdata=measurement['position'], ydata=measurement['coincidence counts'], p0=p0)
 
     fitting = pd.DataFrame(data={'x':measurement['position'], 'y':v_line(measurement['position'], coeff[0], coeff[1], coeff[2], coeff[3], coeff[4])})
@@ -65,6 +68,7 @@ def make_figure(get_selection):
     visibility_spot.plot(kind='scatter', x='position', y='coincidence counts', ax=ax[0], s=3, color='r')
 
     print("Visibility of this data is %.2f"%(visibility*100)+"%")
+    display.config(text="Visibility of this data is %.2f"%(visibility*100)+"%")
 
     ax[0].set_ylim(ymin=0)
     canvas.draw()
@@ -88,10 +92,12 @@ for v in tags['datetime']:
 listbox.pack(side=tk.TOP, fill=tk.BOTH)
 listbox.bind('<<ListboxSelect>>', onSelect)
 
-exit_button = tk.Button(master=widget_frame, text="EXIT", command=exitClick)
-exit_button.pack(side=tk.BOTTOM, fill=tk.BOTH)
+display = tk.Label(master=widget_frame, height=1, width=300, text='asdf')
+display.pack(side=tk.TOP, fill=tk.BOTH)
 
-fig, ax = plt.subplots(3)
+exit_button = tk.Button(master=widget_frame, text="EXIT", command=exitClick)
+exit_button.pack(side=tk.TOP, fill=tk.BOTH)
+
 canvas = FigureCanvasTkAgg(figure=fig, master=figure_frame)
 
 make_figure(get_selection=str(tags.loc[0]['datetime']))
