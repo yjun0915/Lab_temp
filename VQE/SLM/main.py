2825fec6016ab616950f69998799e1ee03b36ff2
yjun0915 <kyjun09150916@gmail.com>
2025-05-21 16:04:39 +0900

    asdf

--- VQE/SLM/main.py
+++ VQE/SLM/main.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 import sympy
+from sympy import Symbol
 from sympy.printing.latex import latex
+
+debug = True
 
 ### DOMAIN ###
 domain = [-10, 10, 500]
@@ -13,14 +16,26 @@ X, Y = np.meshgrid(x, y)
 # hybrid binary fork grating
 
 # Laguerre-Gaussian function
-r = sympy.symbols(names='r', type='real')
-phi = sympy.symbols(names='φ', type='real')
-z = sympy.symbols(names='z', type='real')
-L = sympy.Function('L')(r, phi, z)
-G = sympy.Function('G')(r, phi, z)
+k, w_0, z_R = sympy.symbols(r'k, w_0, z_R')
 
+def _w(z):
+    return w_0*sympy.sqrt(1+(z/z_R)**2)
+w = _w(Symbol('z'))
+if debug: print(w)
+def _R(z):
+    return (z**2 + z_R**2)/z
+R = _R(Symbol('z'))
+if debug: print(R)
+def _G(r, phi, z):
+        return (w_0/w)*sympy.exp(-r**2/w**2)*sympy.exp(-sympy.I*k*z - (sympy.I*k*r**2/(2*R)))
+G = _G(Symbol('r'), Symbol('φ'), Symbol('z'))
+if debug: print(latex(G))
 
-LG = sympy.Function(r'LG^{l}_{p}')(r, phi, z)
+k = 2*sympy.pi*12345.67901235  # cm^-1
+w_0 = 10e-5
+z_R = (w_0**2)*k/2
+
+sympy.plotting.plot3d(G.subs([(z_R, z_R), (w_0, w_0), (k, k)]))
 
 phase = np.exp((X + Y*1j)*2*np.pi/(X**2 + Y**2))
 
