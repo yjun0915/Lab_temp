dee103d3f01309882f90e3eb1fb0a4a191266bc6
yjun0915 <kyjun09150916@gmail.com>
2025-05-22 12:49:54 +0900

    transverse profile of monochromatic Gaussian beam

--- VQE/SLM/main.py
+++ VQE/SLM/main.py
@@ -4,7 +4,7 @@ import sympy
 from sympy import Symbol
 from sympy.printing.latex import latex
 
-debug = True
+debug = False
 
 ### DOMAIN ###
 domain = [-10, 10, 500]
@@ -13,29 +13,36 @@ y = x
 X, Y = np.meshgrid(x, y)
 
 ### FUNCTION ###
+r, phi, z = sympy.symbols('r φ z', real=True)
+w_0, k, z_R = sympy.symbols('w_0 k z_R')
 # hybrid binary fork grating
 
 # Laguerre-Gaussian function
-k, w_0, z_R = sympy.symbols(r'k, w_0, z_R')
-
-def _w(z):
+def _w(w_0, z, z_R):
     return w_0*sympy.sqrt(1+(z/z_R)**2)
-w = _w(Symbol('z'))
-if debug: print(w)
-def _R(z):
+
+def _R(z, z_R):
     return (z**2 + z_R**2)/z
-R = _R(Symbol('z'))
-if debug: print(R)
-def _G(r, phi, z):
+
+def _G(w_0, w, r, k, phi, z, R):
         return (w_0/w)*sympy.exp(-r**2/w**2)*sympy.exp(-sympy.I*k*z - (sympy.I*k*r**2/(2*R)))
-G = _G(Symbol('r'), Symbol('φ'), Symbol('z'))
-if debug: print(latex(G))
 
-k = 2*sympy.pi*12345.67901235  # cm^-1
-w_0 = 10e-5
-z_R = (w_0**2)*k/2
+w = _w(w_0, z, z_R)
+if debug: print(latex(w))
 
-sympy.plotting.plot3d(G.subs([(z_R, z_R), (w_0, w_0), (k, k)]))
+R = _R(z, z_R)
+if debug: print(latex(R))
+
+G = _G(w_0, w, r, k, phi, z, R)
+if debug: sympy.pprint(latex(G))
+
+_k = 2*sympy.pi*12345.67901235  # cm^-1
+_w_0 = 10e-5
+_z_R = (_w_0**2)*_k/2
+
+G = G.subs({w_0:_w_0, k:_k, z_R:_z_R})
+
+sympy.plotting.plot3d(sympy.Abs(G), (r, -1e-3, 1e-3), (z, -1e-3, 1e-3))
 
 phase = np.exp((X + Y*1j)*2*np.pi/(X**2 + Y**2))
 
