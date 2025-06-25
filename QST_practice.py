c8ec67d85482aa80bbadcda1cf251cb7fb332267
yjun0915 <kyjun09150916@gmail.com>
2025-06-25 13:11:45 +0900

    statis

--- QST_practice.py
+++ QST_practice.py
@@ -1,17 +1,10 @@
 import pandas as pd
 
-df = pd.DataFrame(data={
-    'H':[40230, 150, 19970, 20533, 21988, 18910],
-    'V':[220, 39094, 18542, 20050, 18003, 21054],
-    'D':[20556, 19706, 333, 39918, 20475, 19975],
-    'A':[20010, 18629, 38011, 440, 18931, 19544],
-    'R':[19900, 19649, 17623, 21855, 38717, 401],
-    'L':[20761, 19330, 21207, 18804, 483, 39382]
-})
-df.index = ['H', 'V', 'D', 'A', 'R', 'L']
-
-df.to_csv(path_or_buf='./QST_example_2qubit.csv', sep=',')
-
 S = pd.read_csv(filepath_or_buffer='./QST_example_2qubit.csv', sep=',', index_col=0)
+info = S.describe()
 
-print(S)
+normalizer = 1/(info.loc['mean']*6).sum()
+
+S = normalizer*S
+info = S.describe()
+print(info)
