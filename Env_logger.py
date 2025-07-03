ffb109b34884ff437c51f1daa78446da54a9188e
yjun0915 <kyjun09150916@gmail.com>
2025-07-03 14:37:00 +0900

    logging mean data

--- Env_logger.py
+++ Env_logger.py
@@ -1,11 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv(filepath_or_buffer='./env_data.csv', sep=',')
-fig, ax = plt.subplots()
-data['Temperature (C)'].plot(ax=ax)
-(data['Humidity (%)']*45/100).plot(ax=ax)
+# 데이터 불러오기
+df = pd.read_csv('env_data.csv', parse_dates=['Timestamp'])
 
-ax.set_ylim([0, 20])
+# 이중 y축
+fig, ax1 = plt.subplots(figsize=(10, 5))
 
+# 온도용 y축
+color = 'tab:red'
+ax1.set_xlabel('Time')
+ax1.set_ylabel('Temperature', color=color)
+ax1.plot(df['Timestamp'], df['Temperature'], color=color, label='Temp')
+ax1.tick_params(axis='y', labelcolor=color)
+
+# 습도용 y축
+ax2 = ax1.twinx()  # 오른쪽 y축
+color = 'tab:blue'
+ax2.set_ylabel('Humidity', color=color)
+ax2.plot(df['Timestamp'], df['Humidity'], color=color, label='Humidity')
+ax2.tick_params(axis='y', labelcolor=color)
+
+plt.title("Temperature and Humidity Over Time")
+fig.autofmt_xdate()
+plt.tight_layout()
 plt.show()
--- Env_logging.py
+++ Env_logging.py
@@ -1,6 +1,8 @@
+import numpy as np
 import serial
 import time
 import csv
+from datetime import datetime
 
 # 07/03, 13:14~, dt=2s
 ser = serial.Serial('COM14', 9600)
@@ -8,15 +10,27 @@ time.sleep(2)  # 보드 초기화 대기
 
 with open('env_data.csv', 'w', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['Temperature (C)', 'Humidity (%)'])
+    writer.writerow(['Timestamp', 'Temperature', 'Humidity'])
 
+    holder = 30
+    cache = np.zeros(shape=[2, holder])
+    counter = 0
     try:
         while True:
             line = ser.readline().decode().strip()
             if ',' in line:
                 temp, humi = line.split(',')
-                writer.writerow([temp, humi])
-                print(f"Saved: {temp}°C, {humi}%")
+                cache[0][counter] = temp
+                cache[1][counter] = humi
+                cache += 1
+                if cache == holder:
+                    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    ave_temp = np.sum(cache[0][:])/holder
+                    ave_humi = np.sum(cache[1][:])/holder
+                    writer.writerow([timestamp, ave_temp, ave_humi])
+                    cache = np.zeros(shape=[2, holder])
+                    counter = 0
+                print(f"{timestamp} | Temp: {temp}°C, Humidity: {humi}%")
     except KeyboardInterrupt: # 콘솔 좌상단 정지 버튼만 작동함
         print("데이터 저장 종료")
     finally:
