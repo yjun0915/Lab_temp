45d93b8b4de0037bd3fc8f15905b5b31ce860166
yjun0915 <kyjun09150916@gmail.com>
2025-07-03 14:38:19 +0900

    typo

--- Env_logging.py
+++ Env_logging.py
@@ -23,14 +23,14 @@ with open('env_data.csv', 'w', newline='') as f:
                 cache[0][counter] = temp
                 cache[1][counter] = humi
                 cache += 1
-                if cache == holder:
+                if counter == holder:
                     timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     ave_temp = np.sum(cache[0][:])/holder
                     ave_humi = np.sum(cache[1][:])/holder
                     writer.writerow([timestamp, ave_temp, ave_humi])
                     cache = np.zeros(shape=[2, holder])
                     counter = 0
-                print(f"{timestamp} | Temp: {temp}°C, Humidity: {humi}%")
+                print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | Temp: {temp}°C, Humidity: {humi}%")
     except KeyboardInterrupt: # 콘솔 좌상단 정지 버튼만 작동함
         print("데이터 저장 종료")
     finally:
