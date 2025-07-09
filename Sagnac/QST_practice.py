5e6646fda8e13d1443ec34ebdcfa8a2b3050acc7
yjun0915 <kyjun09150916@gmail.com>
2025-07-09 12:39:09 +0900

    [OFFICE] minor commit

--- Sagnac/QST_practice.py
+++ Sagnac/QST_practice.py
@@ -102,10 +102,10 @@ for i in range(4):
                                  operator[stocks_index[j][0]] + (stocks_index[j][3]*operator[stocks_index[j][1]]))
 
 target = np.array([
-    [0.25, 0.25, 0, 0],
-    [0.25, 0.25, 0, 0],
-    [0, 0, 0.25, 0.25],
-    [0, 0, 0.25, 0.25]
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
 ])
 
 fidelity = np.trace(sqrtm(sqrtm(output).dot(target.dot(sqrtm(output)))))**2
